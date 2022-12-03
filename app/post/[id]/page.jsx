c3c122ac41528c4499e9c1dfc9200b8fc5177f96
leosmar <zoidpvp@gmail.com>
2022-12-02 22:00:21 -0400

    prueba1

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import getPostData from "./getPostData";
+import getPostData from "../../../lib/getPostData";
 import styles from "./post.module.css";
 
 
 export default async function Post({ params }) {
   let { id } = params;
-
   const post = await getPostData(id);
 
   return (
