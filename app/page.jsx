c8511b6f25dd154c08716f519d236aa9a9960f44
leosmar <zoidpvp@gmail.com>
2022-12-02 21:55:24 -0400

    errors

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,8 @@
 import styles from "./HomePage.module.css";
 import ArticleCard from "../components/ArticleCard/ArticleCard";
 import getPosts from "../lib/getPosts";
-import getPostData from "./post/[id]/getPostData";
 
-export default async function HomePage() {
-  const post = await getPostData(1);
+export default function HomePage() {
   return (
     <>
       <header className={styles.header}>
@@ -12,9 +10,7 @@ export default async function HomePage() {
           <i>Learn english</i> <br /> UPT Jose Felix Ribas
         </h1>
       </header>
-
-      <div dangerouslySetInnerHTML={{ __html: post.contentHtml }}></div>
-
+    
       <section className={styles.contentArticle}>
         {getPosts.map(({ id, title, content }) => {
           return <ArticleCard id={id} title={title} content={content} />;
