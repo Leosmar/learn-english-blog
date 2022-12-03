2fde54a386f1e8cb4c7638c7cab07d96536a65e9
leosmar <zoidpvp@gmail.com>
2022-12-02 21:49:25 -0400

    error

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,19 @@
-"use client"
-
 import styles from "./HomePage.module.css";
 import ArticleCard from "../components/ArticleCard/ArticleCard";
 import getPosts from "../lib/getPosts";
+import getPostData from "./post/[id]/getPostData";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const post = await getPostData(1);
   return (
     <>
       <header className={styles.header}>
         <h1 className={styles.title}>
           <i>Learn english</i> <br /> UPT Jose Felix Ribas
         </h1>
-        {/* <span className={styles.containerBackImage}>
-      <Image className={styles.backImage} src={backImage} />
-    </span> */}
       </header>
+
+      <div dangerouslySetInnerHTML={{ __html: post.contentHtml }}></div>
 
       <section className={styles.contentArticle}>
         {getPosts.map(({ id, title, content }) => {
