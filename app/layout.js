0103d12f759eb4526b613a1d1d7494bd02b0dee5
leosmar <zoidpvp@gmail.com>
2022-12-04 01:21:32 -0400

    add responsivity whit tailwindcss

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,25 @@ export default function RootLayout({ children }) {
     <html>
       <head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+        <meta name="description" content="" />
+        <meta
+          name="viewport"
+          content="width=device-width,minimum-scale=1,initial-scale=1"
+        />
+        <meta property="og:title" content="Blog ingles - articulos" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="Blog ingles - estudiantes UPT" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="es_ES" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@leosmar_delgado" />
+        <meta name="twitter:title" content="Blog ingles - articulos" />
         <title>English blog</title>
       </head>
       <body className={SpaceGroteskTitle.variable}>
-        <main className={styles.containerLayout}>{children}</main>
+        <main className="block w-full max-w-6xl p-4 pb-32 m-auto">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- components/ArticleCard/ArticleCard.jsx
+++ components/ArticleCard/ArticleCard.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import React from "react";
 import styles from "./ArticleCard.module.css";
@@ -6,7 +6,10 @@ import Link from "next/link";
 
 export default function ArticleCard({ id, title, content }) {
   return (
-    <Link className="relative h-full p-6 transition-all rounded-lg hover:scale-105 hover:bg-sky-100 shadow-box" href={`/post/${id}`}>
+    <Link
+      className={`relative h-full p-6 transition-all rounded-lg hover:scale-105 hover:bg-sky-100 shadow-lg ${styles.shadowBox}`}
+      href={`/post/${id}`}
+    >
       <article className="flex flex-col justify-center gap-y-4">
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.article}>{content}</p>
